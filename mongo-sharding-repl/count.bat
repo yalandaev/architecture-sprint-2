ad6c47bee2baf8a0bd3c751a470765460399a0a5
Evgeniy Yalandaev <e.yalandaevx@dodopizza.com>
2024-10-25 00:53:18 +0400

    task 3

--- mongo-sharding-repl/count.bat
+++ mongo-sharding-repl/count.bat
@@ -1,3 +1,3 @@
-docker exec -i mongos_router_2 mongosh --port 27021 --eval "use('somedb'); print('All documents: ' + db.helloDoc.countDocuments());"
-docker exec -i shard1 mongosh --port 27018 --eval "use('somedb'); print('shard1: ' + db.helloDoc.countDocuments());"
-docker exec -i shard2 mongosh --port 27019 --eval "use('somedb'); print('shard2: ' + db.helloDoc.countDocuments());"
+docker exec -i mongos_router_2 mongosh --port 27025 --eval "use('somedb'); print('All documents: ' + db.helloDoc.countDocuments());"
+docker exec -i shard1-rep1 mongosh --port 27018 --eval "use('somedb'); print('shard1: ' + db.helloDoc.countDocuments());"
+docker exec -i shard2-rep1 mongosh --port 27021 --eval "use('somedb'); print('shard2: ' + db.helloDoc.countDocuments());"
